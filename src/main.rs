11c7bb978b87a2b46270227a6e4bf45b22dc08ad
Elanis <elanis@hotmail.com>
2020-11-27 21:15:31 +0100

    Chapter 2: The vec3 class

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,24 @@
+mod vec3;
+
+use vec3::Vec3;
+
 fn main() {
-    let nx = 200;
-    let ny = 100;
+	let nx = 200;
+	let ny = 100;
 
-    println!("P3");
-    println!("{}", nx.to_string() + &" ".to_owned() + &ny.to_string());
-    println!("255");
+	println!("P3");
+	println!("{}", nx.to_string() + &" ".to_owned() + &ny.to_string());
+	println!("255");
 
-    for j in (0..ny).rev() {
-    	for i in 0..nx {
-    		let r = i as f64 / nx as f64;
-    		let g = j as f64 / ny as f64;
-    		let b = 0.2 as f64;
+	for j in (0..ny).rev() {
+		for i in 0..nx {
+			let col = Vec3::new(i as f32 / nx as f32, j as f32 / ny as f32, 0.2 as f32);
 
-    		let ir = (255.99*r).round();
-    		let ig = (255.99*g).round();
-    		let ib = (255.99*b).round();
+			let ir = (255.99*col.x).round();
+			let ig = (255.99*col.y).round();
+			let ib = (255.99*col.z).round();
 
-    		println!("{}", ir.to_string() + &" ".to_owned() + &ig.to_string() + &" ".to_owned() + &ib.to_string());
-    	}
-    }
+			println!("{}", ir.to_string() + &" ".to_owned() + &ig.to_string() + &" ".to_owned() + &ib.to_string());
+		}
+	}
 }
